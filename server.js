1d07b46bcaece4fc1c4ee5df831add2a881a4dfd
DARAZOUKI <Aussama@chalmers.se>
2024-04-03 16:02:06 +0200

    change1

--- server.js
+++ server.js
@@ -115,6 +115,6 @@ app.get('/about', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(10000, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
